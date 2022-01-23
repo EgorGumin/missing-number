ab1151963b9a3a3e01d9cdad0dee0f9887ca531b
Gumin Egor <gumin.egor@gmail.com>
2022-01-24 01:19:39 +0300

    Add function body

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def find_missed_num(numbers: List[int]) -> int:
+    """ Searches for the missing number in a shuffled series
+
+    :param numbers: a shuffled series 1..n with one missing number
+    :return: int: Missing number
+    """
+    pass
